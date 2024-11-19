c12583f5681cc27004d0edd086d88c3ecbf20225
Ícaro Rangel <icarogin2@gmail.com>
2024-11-19 17:57:25 -0400

    First Commit

---
+++ MetéoApp/Mete_oAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mete_oAppApp.swift
+//  MetéoApp
+//
+//  Created by icaro rangel on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Mete_oAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
